6b754a5319fb1ffbe06460912b710b8e39fcac10
ink3rroll <ink3rroll@gmail.com>
2026-03-19 09:29:37 +0800

    feat: connected exerciselist get to api

---
+++ src/api/apiExercises.js
@@ -0,0 +1,7 @@
+const BASE_URL = "http://localhost:3000"
+
+export const getExercises = async () => {
+    const res = await fetch(`${BASE_URL}/exercises`)
+    if (!res.ok) throw new Error(`Error fetching data: ${res.status}`)
+    return await res.json()
+}
